ce040cf4a47602be5a8d51b81ed3d146c0004521
mj4617384-art <mj4617384@gmail.com>
2026-08-07 01:39:11 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { supabase } from '../supabaseClient';
+import { supabase } from '../lib/supabase';
 import {
   Home as HomeIcon,
   MessageCircle,
