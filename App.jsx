8bbf9cc1a3090fb421d703d940a4888e0ffd30a9
mj4617384-art <mj4617384@gmail.com>
2026-08-10 12:21:59 +0100

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
 import Splash from './pages/Splash';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
@@ -15,14 +14,10 @@ export default function App() {
         <Route path="/" element={<Splash />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
-
         <Route path="/home" element={<Home />} />
-
         <Route path="/profile" element={<Profile />} />
         <Route path="/profile/:userId" element={<Profile />} />
-
         <Route path="/menu" element={<Menu />} />
-
         <Route path="/discover" element={<Discover />} />
       </Routes>
     </BrowserRouter>
